43a32f73047bec37c08b883bad85b7f959ee4dba
15217613 <15217613@uagro.mx>
2025-10-29 20:08:45 -0600

    FIXED: Arreglado tamaño responsivo en celulares

--- BOOTSTRAP/TRANSPORTE/igsforwarding - onepage/assets/js/main.js
+++ BOOTSTRAP/TRANSPORTE/igsforwarding - onepage/assets/js/main.js
@@ -109,12 +109,17 @@ function initializeApp() {
         let current = '';
         const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
         
+        // Recorrer todas las secciones del documento
         sections.forEach(section => {
+            // Obtener la posición de la sección desde el inicio de la página y su altura
             const sectionTop = section.offsetTop;
             const sectionHeight = section.clientHeight;
             
-            // Si estamos en la sección (considerando el navbar de 70px)
+            // Si estamos en la sección (considerando el navbar de 100px)
+            // La condición se cumple cuando la posición de la ventana está entre la posición de la sección
+            // menos 100px y la posición de la sección más su altura menos 100px
             if (scrollPosition >= (sectionTop - 100) && scrollPosition < (sectionTop + sectionHeight - 100)) {
+                // Almacenar el ID de la sección actual
                 current = section.getAttribute('id');
             }
         });
